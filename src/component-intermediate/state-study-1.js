ed60444cfa613ea3f32900bb77e3c1959975fc09
vinszip999 <w2011@e-mirim.hs.kr>
2022-03-30 11:25:06 +0900

    220330 수업 (전개 문법을 사용한 객체 복사 복습, 세터 함수 응용 예제)

--- src/component-intermediate/state-study-1.js
+++ src/component-intermediate/state-study-1.js
@@ -1,18 +1,19 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
 
-function Counter(props){
+function Counter(props) {
     const [count, setCount] = useState(0) // React.useState(0)로 바로 React. 붙여서 import 해줘도 됨.
     // const [ myname, setMyName ] = useState("John") // set을 붙여야 한다.
 
     return (
         <div>
             <h1>{count}</h1>
-            {/* <button onclick={() => setCount(count + 1)}>증가</button>  html 문법 방식!!*/}
-            
-            <button onClick={() => setCount(count + 1)}>증가</button>
-            <button onClick={() => setCount(count - 1)}>감소</button>
+            {/* <button onclick={() => setCount(count + 1)}>증가</button>  html 문법 방식!! */}
+            <button onClick={() => setCount(prev => prev + 1)}>증가</button> {/* 화살표 함수 */}
+
+            <button onClick={() => setCount(function (prev) { return prev - 1 })}>감소</button> {/*이런 형태로 써주는 것이 좋다!*/}
+            {/* <button onClick={() => setCount(count - 1)}>감소</button> */}
 
             {/* <button onkeypress={() => setCount(count + 1)}>증가</button> html 문법 방식!!*/}
             {/* <button onKeyPress={() => setCount(count + 1)}>증가</button> */}
@@ -21,7 +22,7 @@ function Counter(props){
     )
 }
 ReactDOM.render(
-    <Counter/>
+    <Counter />
     ,
     document.getElementById("root")
 );
