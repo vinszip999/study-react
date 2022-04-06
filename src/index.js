c3e6b2b9a5e8b7a0da3dfd3d01f2fce2918c3cb4
vinszip999 <w2011@e-mirim.hs.kr>
2022-04-06 12:16:50 +0900

    220406 수업 (의존 배열 활용 예제 실습, 정리(clean up) 작업 진행)

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 // import './jsxstudy/jsx-study1';
+
 // import './component-basic/first-component';
 // import './component-basic/props-basic';
 // import './component-basic/props-children';
 // import './component-basic/slotMachine_study';
+
 // import './component-intermediate/state-study-1';
 // import './component-intermediate/state-study-2';
 // import './component-intermediate/state-study-with-inner-object';
 // import './component-intermediate/counter-demo';
+
 // import './js-study/immutable-list-manipulation';
+
 // import './component-intermediate/manipulate-list';
 // import './component-intermediate/todo-app-demo';
 // import './component-intermediate/test-todo-app';
 
-import './use-effect-demo/use-effect-intro';
+// import './use-effect-demo/use-effect-intro';
+// import './use-effect-demo/use-effect-deps';
+// import './use-effect-demo/use-effect-using-local-storage';
+// import './use-effect-demo/use-effect-unmount';
+import './use-effect-demo/use-effect-unmount-deps';
